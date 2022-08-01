0f1d2669ef498780c0eeea20a43db6f8622d9c2d
Renan <renan.fachin@hotmail.com>
2022-08-01 18:52:36 -0300

    Utilizando os parâmetros na rota, utilizando o resquest params para capturar os dados e feito a desestruturação do código

--- src/server.js
+++ src/server.js
@@ -6,8 +6,13 @@ const express = require("express");
 const app = express();
 
 // Implementando o método GET para quando for solicitado no /message da porta 3333
-app.get("/message", (request, response) => {
-    response.send("Hello, world")
+app.get("/message/:id/:user", (request, response) => {
+    const { id, user } = request.params // Fazendo a desestruturação do código para evitar a duplicitade
+
+    response.send(`
+        Mensagem ID: ${id}.
+        Para o usuário: ${user}.
+        `) // request.params. é a forma para se capturar um parâmetro enviado pelo método GET
 })
 
 // Criando uma porta para atender as solicitações
