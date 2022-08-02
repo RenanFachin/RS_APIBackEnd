bd44f6d319dfb895c634c9c5ca1fb29654065ec5
Renan <renan.fachin@hotmail.com>
2022-08-02 16:38:40 -0300

    Método POST. Definindo como JSON o tipo de dado padrão enviado pelo método POST e obtendo as informações pelo request.body

--- src/server.js
+++ src/server.js
@@ -5,23 +5,34 @@ const express = require("express");
 // Inicializando o express
 const app = express();
 
+// Definindo o padrão de entrada dos dados como dado do tipo JSON
+app.use(express.json());
+
 // Implementando o método GET para quando for solicitado no /message da porta 3333
 // Route Params = (VALORES OBRIGATÓRIOS)
-app.get("/message/:id/:user", (request, response) => {
-    const { id, user } = request.params // Fazendo a desestruturação do código para evitar a duplicitade
+// app.get("/message/:id/:user", (request, response) => {
+//     const { id, user } = request.params // Fazendo a desestruturação do código para evitar a duplicitade
 
-    response.send(`
-        Mensagem ID: ${id}.
-        Para o usuário: ${user}.
-        `) // request.params. é a forma para se capturar um parâmetro enviado pelo método GET
-})
+//     response.send(`
+//         Mensagem ID: ${id}.
+//         Para o usuário: ${user}.
+//         `) // request.params. é a forma para se capturar um parâmetro enviado pelo método GET
+// })
 
 // Query Params = (VALORES OPCIONAIS)
-app.get("/users", (request, response) => {
-    const { page, limit} = request.query; // Desestruturação do request.query
+// app.get("/users", (request, response) => {
+//     const { page, limit} = request.query; // Desestruturação do request.query
 
-    response.send(`Página: ${page}. Mostrar: ${limit}`);
-});
+//     response.send(`Página: ${page}. Mostrar: ${limit}`);
+// });
+
+// MÉTODO POST
+app.post("/users", (request, response) => {
+    // Obtendo as informações passadas pelo método POST
+    const { name, email, password } = request.body;
+
+    response.json({ name, email, password }); // resposta no modo de objeto (json)
+})
 
 
 
