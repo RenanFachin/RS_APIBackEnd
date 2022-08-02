52434d5e9c12c05a9da6109a1efccb15bb2229bc
Renan <renan.fachin@hotmail.com>
2022-08-02 17:50:02 -0300

    Uso de Middleware na aplicação

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,19 +1,36 @@
 // importando o app
 const { Router } = require("express");
 
-
 // Importando o controller
 const UsersController = require("../controllers/UsersController");
 
-
 const usersRoutes = Router();
+
+function myMiddleware(request, response, next){
+    console.log("Você passou pelo middleware!");
+    
+    // Caso o usuário não seja um admin
+    if(!request.body.isAdmin){ // Obtendo as informações enviadas na requisição
+        return response.json({ message: "user unatuthorized" });
+    }
+
+    next();
+}
+
+
+
+
+
+
+
+
 
 // Instanciando
 const userController = new UsersController();
 
 
 // MÉTODO POST
-usersRoutes.post("/", userController.create);
+usersRoutes.post("/", myMiddleware, userController.create);
 
 // Exportando
 module.exports = usersRoutes;
