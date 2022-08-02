a515a842e0e7755de628aef11306c0d7342bc1b4
Renan <renan.fachin@hotmail.com>
2022-08-02 17:50:29 -0300

    Retirado a função de exemplo do Middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -6,31 +6,13 @@ const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router();
 
-function myMiddleware(request, response, next){
-    console.log("Você passou pelo middleware!");
-    
-    // Caso o usuário não seja um admin
-    if(!request.body.isAdmin){ // Obtendo as informações enviadas na requisição
-        return response.json({ message: "user unatuthorized" });
-    }
-
-    next();
-}
-
-
-
-
-
-
-
-
 
 // Instanciando
 const userController = new UsersController();
 
 
 // MÉTODO POST
-usersRoutes.post("/", myMiddleware, userController.create);
+usersRoutes.post("/", userController.create);
 
 // Exportando
 module.exports = usersRoutes;
