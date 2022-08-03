01c7b4ac04bf7ad5fd093c7f4babf6e5ea305f65
Renan <renan.fachin@hotmail.com>
2022-08-03 15:44:27 -0300

    Criado método para alteração de senha

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,5 +1,5 @@
 // IMPORTANDO criptografia
-const { hash } = require ("bcryptjs"); 
+const { hash, compare } = require ("bcryptjs"); 
 
 // Importando a AppError
 const AppError = require("../utils/AppError");
@@ -46,7 +46,7 @@ class UsersController {
     }
 
     async update(request, response) {
-        const { name, email } = request.body;
+        const { name, email, password, old_password } = request.body;
         const { id } = request.params;
 
         const database = await sqliteConnection();
@@ -69,13 +69,32 @@ class UsersController {
         user.name = name;
         user.email = email;
 
+        // ALTERANDO A SENHA
+        // Se não for informado a senha antiga:
+        if( password && !old_password){
+            throw new AppError("Você precisa informar a senha antiga para poder criar uma nova senha.");
+        }
+
+        // Se tanto a nova senha quanto a senha antiga forem informados
+        if( password && old_password){
+            const checkOldPassword = await compare(old_password, user.password); 
+            // user.password = password de dentro do usuário
+
+            if(!checkOldPassword){
+                throw new AppError("A senha antiga não confere.");
+            }
+
+            user.password = await hash(password,8);
+        }
+
         await database.run(`
             UPDATE users SET
             name = ?,
             email = ?,
+            password = ?,
             updated_at = ?
             WHERE id = ?`,
-            [user.name, user.email, new Date(), id]
+            [user.name, user.email, user.password, new Date(), id]
             );
         
         return response.status(200).json();
