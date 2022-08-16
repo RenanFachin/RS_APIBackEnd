39e180bd9b1ff97a5664f63e4e4f3744acf933b0
Renan <renan.fachin@hotmail.com>
2022-08-16 15:47:45 -0300

    Validação do Password informado pelo usuário

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -4,6 +4,9 @@ const knex = require ("../database/knex");
 // Importando mensagens de erro
 const AppError = require("../utils/AppError");
 
+// Importando uma função do BCRYPT para fazer a comparação das senhas criptografadas
+const { compare } = require("bcryptjs");
+
 class SessionsController {
     async create(request, response){
         const { email, password } = request.body
@@ -15,6 +18,16 @@ class SessionsController {
         // Fazendo a validação
         // Caso o usuário não exista
         if (!user){
+            throw new AppError("E-mail e/ou senha incorreta", 401);
+        }
+
+        // Fazendo a comparação da senha digitada com a senha que tem cadastrada no banco de dados
+        // É aproveitado o user.password porque o user já foi até o banco de dados e já tem esse dado
+        const passwordMatched = await compare(password, user.password)
+
+
+        // Fazendo a verificação da senha digitada
+        if(!passwordMatched){
             throw new AppError("E-mail e/ou senha incorreta", 401);
         }
 
