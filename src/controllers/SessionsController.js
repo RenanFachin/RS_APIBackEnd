fc28630069baf7b571727f6ad825898a1cebb52e
Renan <renan.fachin@hotmail.com>
2022-08-16 15:37:19 -0300

    Validação de usuário com o banco de dados

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,9 +1,25 @@
+// Importando a conexão com o banco de dados
+const knex = require ("../database/knex");
+
+// Importando mensagens de erro
+const AppError = require("../utils/AppError");
+
 class SessionsController {
     async create(request, response){
         const { email, password } = request.body
 
-        
-         return response.json({email, password})
+        const user = await knex("users").where({email}).first();
+        // Usando o knex para acessar a tabela de users e fazer um filtro de email
+
+
+        // Fazendo a validação
+        // Caso o usuário não exista
+        if (!user){
+            throw new AppError("E-mail e/ou senha incorreta", 401);
+        }
+
+        // testando
+        return response.json(user)
     }
 }
 
