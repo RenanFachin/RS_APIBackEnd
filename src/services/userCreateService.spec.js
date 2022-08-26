1639129ad2e350a58d23c54f951b881d1ec4044f
Renan <renan.fachin@hotmail.com>
2022-08-26 10:33:18 -0300

    Cria um teste automatizado que verifica se vai ser rejeitada a criação de um novo usuário com um email já existente

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -1,6 +1,6 @@
 const UserCreateService = require("./UserCreateService");
 const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory");
-
+const AppError = require("../utils/AppError");
 
 // o async está aqui para poder usr o await dentro da função para simular exatamente o funcionamento da aplicação
 describe("UserCreateService", () =>{
@@ -17,13 +17,32 @@ describe("UserCreateService", () =>{
         const userCreated = await userCreateService.execute(user)
         
         //debugando
-        console.log(userCreated)
+        // console.log(userCreated)
     
         // Espero que o usuário de exemplo criado tenha um objeto com uma propriedade .id
         expect(userCreated).toHaveProperty("id")
     })
 
-    it("", () => {
-        
+    it("user not should be create with exists email ", async () => {
+        const user1 = {
+            name: "User Teste 1",
+            email: "user@test.com",
+            password: "123"
+        }
+
+        const user2 = {
+            name: "User Teste 2",
+            email: "user@test.com",
+            password: "456"
+        }
+
+        const userRepository = new UserRepositoryInMemory()
+        const userCreateService = new UserCreateService(userRepository)
+
+        await userCreateService.execute(user1)
+
+        // Verificando se vai ser rejeitada a criação de um usuário com email já existente
+        await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError("Este e-mail já está em uso."))
+
     })
 })
